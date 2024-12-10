3d15a6e97a08a009b98ec8a3cfb5046105656acd
rohith26102006 <rohith26103006@gmail.com>
2024-12-10 08:48:56 +0000

    practical1

--- Practical1.java
+++ Practical1.java
@@ -1,35 +1,32 @@
-class person
+class Person
 {
     String firstName;
     String lastName;
-    int age;
-
-    public person()
+    int age;;
+    public Person()
     {
-        firstName= "";
-        lastName= "";
-        age=0;
+        firstName = "";
+        lastName = "";
+        age = 0;
     }
-     public person(String firstName, String lastName,int age)
-    {
-        this.firstName=firstName;
-        this.lastName=lastName;
-        this.age=age;
+    public Person(String firstName,String lastName,int age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
     }
-     String getFullName() {
-     return firstName + "" + lastName;
+    public String getFullName(){
+    return firstName + " " + lastName;
     }
-}
+    }
     public class Practical1
     {
     public static void main(String[] args)
     {
-        person person1 =new person("John", "Doe", 30);
-        person person2 =new person("Alice", "Smith",25);
-        System.out.println("person 1:" + person1.getFullName());
-        System.out.println("person 2:" + person2.getFullName());
-        double averageAge=(person1.age+ person2.age) / 2.0;
-        System.out.println("Average Age: " + averageAge);
+    Person person1 = new Person("John","Doe",30);
+    Person person2 = new Person("Alice","Smith",25);
+    System.out.println("Person1: " + person1.getFullName());
+    System.out.println("Person2: " + person2.getFullName());
+    double averageAge = (person1.age + person2.age)/2.0;
+    System.out.println("Average age: " + averageAge);
+        }
     }
-}
-
